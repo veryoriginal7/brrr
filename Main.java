4133a1b1bc0f4145ec34f13da705fa520478a39c
veryoriginal7 <tuandatle2005@gmail.com>
2022-12-04 11:06:40 -0800

    add a bunch of stuff

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
-
+public class Main{
+    public static void printSlogan(String newSlogan){
+        System.out.println(newSlogan);
+    }
+     public static void main (String args[]){
+SuperHero notDicks= new SuperHero();
+notDicks.setName("Bruce Wayne");
+notDicks.setAge(47);
+notDicks.setWeakness("Letting joker lives so many times");
+notDicks.setSuperPower("a lot of money");
+System.out.println("This super hero name is "+notDicks.getName()+"his age is "+ notDicks.getAge()+"  his super power is "+ notDicks.getSuperPower()+" his weakness is "+ notDicks.getWeakness());
+        printSlogan("yeaaaaa yeah");
+    }
+    /*  7A.) a repository is where the class and code get store( I think)
+        7b.) because we can send code to each other and add that code to a project
+        7C.) A commit is a like a picture or a spawnpoint of a code that you can't chaange, you can add that to your branch
+        7D.)idk
+        7E.) Idk
+        7F.)you can make a copy of your code, then merge it back to the main branch.
+        7G.) idk
+    */
+}
--- SuperHero.java
+++ SuperHero.java
@@ -1,1 +1,43 @@
+public class SuperHero{
+    private int age;
+    private String name;
+    private String superPower;
+    private String weakness;
+public SuperHero(){
+    age= 5;
+    name="Dicks";
+    superPower="adopted son of a rich guy";
+    weakness="no power";
+}
+public SuperHero(int nAge, String nName, String nSuperPower, String nWeakness){
+age=nAge;
+name=nName;
+superPower=nSuperPower;
+weakness=nWeakness;
+}
+    public void setAge(int nAge){
+    age=nAge;
+}
+public void setName(String nName){
+    name=nName;
+}
+public void setSuperPower(String nPower){
+    superPower=nPower;
+}
+public void setWeakness (String nWeak){
+    weakness=nWeak;
+}
+public int getAge(){
+    return age;
+}
+public String getName(){
+    return name;
+}
+public String getSuperPower(){
+    return superPower;
+}
+public String getWeakness(){
+    return weakness;
+}
 
+}
